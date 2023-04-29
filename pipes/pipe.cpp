4e6cc369aaa56642ebb684005994a3c5eca01b8d
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2023-04-29 15:37:47 -0300

    fix prime function on pipe

--- pipes/pipe.cpp
+++ pipes/pipe.cpp
@@ -26,34 +26,42 @@ int main()
         std::cerr << "Error: failed to create pipe" << std::endl;
         return 1;
     }
-    for (int i = 0; i < 100; i++)
+    pid_t pid = fork();
+    if (pid < 0)
     {
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            std::cerr << "Error: failed to fork" << std::endl;
-            return 1;
-        }
-        else if (pid == 0)
-        {
-            // Consumer
-            close(pipefd[1]);
-            read(pipefd[0], buffer, sizeof(buffer));
+        std::cerr << "Error: failed to fork" << std::endl;
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        // Consumer
+        close(pipefd[1]);
+        read(pipefd[0], buffer, sizeof(buffer));
+        close(pipefd[0]);
 
-            is_prime(n);
-            std::cout << "Child received message: " << buffer << std::endl;
-            close(pipefd[0]);
-            return 0;
+        int num = std::stoi(buffer);
+        std::string response;
+        if (is_prime(num))
+        {
+            response = " is prime";
         }
         else
         {
-            // Producer
-            close(pipefd[0]);
-            srand(time(0));
-            std::string message = std::to_string(rand() % 100);
-            write(pipefd[1], message.c_str(), message.size());
-            close(pipefd[1]);
+            response = " is not prime";
         }
+
+        std::cout << "The number " << buffer << response << std::endl;
+        return 0;
+    }
+    else
+    {
+        // Producer
+        close(pipefd[0]);
+        srand(time(0));
+        int num = rand() % 100;
+        std::string message = std::to_string(num);
+        write(pipefd[1], message.c_str(), message.size());
+        close(pipefd[1]);
     }
     return 0;
 }
