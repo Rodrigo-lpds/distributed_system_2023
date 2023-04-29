985e9c75d729eccc3866e7b02bbd98e9d69d7638
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2023-04-29 16:14:06 -0300

    add number of times to run pipe program

--- pipes/pipe.cpp
+++ pipes/pipe.cpp
@@ -19,49 +19,56 @@ int main()
 {
     int pipefd[2];
     char buffer[1024];
-    int n = 1;
+    int n;
 
-    if (pipe(pipefd) < 0)
-    {
-        std::cerr << "Error: failed to create pipe" << std::endl;
-        return 1;
-    }
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        std::cerr << "Error: failed to fork" << std::endl;
-        return 1;
-    }
-    else if (pid == 0)
-    {
-        // Consumer
-        close(pipefd[1]);
-        read(pipefd[0], buffer, sizeof(buffer));
-        close(pipefd[0]);
+    std::cout << "Type the number of times the program will run: ";
+    std::cin >> n;
 
-        int num = std::stoi(buffer);
-        std::string response;
-        if (is_prime(num))
+    for (int i = 0; i < n; i++) // Executar 5 vezes
+    {
+        if (pipe(pipefd) < 0)
         {
-            response = " is prime";
+            std::cerr << "Error: failed to create pipe" << std::endl;
+            return 1;
+        }
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            std::cerr << "Error: failed to fork" << std::endl;
+            return 1;
+        }
+        else if (pid == 0)
+        {
+            // Consumer
+            close(pipefd[1]);
+            read(pipefd[0], buffer, sizeof(buffer));
+            close(pipefd[0]);
+
+            int num = std::stoi(buffer);
+            std::string response;
+            if (is_prime(num))
+            {
+                response = " is prime";
+            }
+            else
+            {
+                response = " is not prime";
+            }
+
+            std::cout << "The number " << buffer << response << std::endl;
+            return 0;
         }
         else
         {
-            response = " is not prime";
+            // Producer
+            close(pipefd[0]);
+            srand(time(0));
+            int num = rand() % 100;
+            std::string message = std::to_string(num);
+            sleep(1);
+            write(pipefd[1], message.c_str(), message.size());
+            close(pipefd[1]);
         }
-
-        std::cout << "The number " << buffer << response << std::endl;
-        return 0;
-    }
-    else
-    {
-        // Producer
-        close(pipefd[0]);
-        srand(time(0));
-        int num = rand() % 100;
-        std::string message = std::to_string(num);
-        write(pipefd[1], message.c_str(), message.size());
-        close(pipefd[1]);
     }
     return 0;
 }
