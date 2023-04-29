3d74f27bb50b04fd0a7871f1a6532a16e3614c28
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2023-04-29 17:03:41 -0300

    stop consumer<->producer connection when number 0 is sent

--- sockets/consumer.cpp
+++ sockets/consumer.cpp
@@ -25,62 +25,71 @@ int main()
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     const char *response;
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    while (true)
     {
-        std::cerr << "Error: socket failed" << std::endl;
-        return 1;
-    }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        std::cerr << "Error: setsockopt failed" << std::endl;
-        return 1;
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8081);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        std::cerr << "Error: bind failed" << std::endl;
-        return 1;
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        std::cerr << "Error: listen failed" << std::endl;
-        return 1;
-    }
-
-    std::cout << "Waiting for connections..." << std::endl;
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-    {
-        std::cerr << "Error: accept failed" << std::endl;
-        return 1;
-    }
-
-    std::cout << "Connection established" << std::endl;
-
-    int valread;
-    while ((valread = read(new_socket, buffer, 1024)) > 0)
-    {
-        std::cout << "Random number received: " << buffer << std::endl;
-        int num = std::stoi(buffer);
-        if (is_prime(num))
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
         {
-            response = "The number is prime";
+            std::cerr << "Error: socket failed" << std::endl;
+            return 1;
         }
-        else
-        {
-            response = "The number is not prime";
-        }
-        send(new_socket, response, strlen(response), 0);
-        memset(buffer, 0, sizeof(buffer));
-    }
 
-    close(new_socket);
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+        {
+            std::cerr << "Error: setsockopt failed" << std::endl;
+            return 1;
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(8081);
+
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+        {
+            std::cerr << "Error: bind failed" << std::endl;
+            return 1;
+        }
+
+        if (listen(server_fd, 3) < 0)
+        {
+            std::cerr << "Error: listen failed" << std::endl;
+            return 1;
+        }
+
+        std::cout << "Waiting for connections..." << std::endl;
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            std::cerr << "Error: accept failed" << std::endl;
+            return 1;
+        }
+
+        std::cout << "Connection established" << std::endl;
+
+        int valread;
+        while ((valread = read(new_socket, buffer, 1024)) > 0)
+        {
+            std::cout << "Random number received: " << buffer << std::endl;
+            int num = std::stoi(buffer);
+            if (num == 0)
+            {
+                std::cout << "Client disconnected" << std::endl;
+                close(new_socket);
+                return 0;
+            }
+
+            if (is_prime(num))
+            {
+                response = "The number is prime";
+            }
+            else
+            {
+                response = "The number is not prime";
+            }
+            send(new_socket, response, strlen(response), 0);
+            memset(buffer, 0, sizeof(buffer));
+        }
+
+        close(new_socket);
+    }
     return 0;
 }
--- sockets/producer.cpp
+++ sockets/producer.cpp
@@ -4,12 +4,14 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         std::cerr << "Error: failed to create socket" << std::endl;
         return 1;
     }
@@ -17,33 +19,39 @@ int main(int argc, char const *argv[]) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(8081);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
         std::cerr << "Error: failed to convert address" << std::endl;
         return 1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         std::cerr << "Error: failed to connect to server" << std::endl;
         return 1;
     }
+    while (true)
+    {
+        srand(time(nullptr));
+        int random_number = rand() % 100;
+        std::string message = std::to_string(random_number);
 
-    srand(time(nullptr));
-    int random_number = rand() % 100;
-    std::string message = std::to_string(random_number);
+        if (send(sock, message.c_str(), message.length(), 0) < 0)
+        {
+            std::cerr << "Error: failed to send message" << std::endl;
+            return 1;
+        }
+        std::cout << "Number sent to server" << std::endl;
 
-    if (send(sock, message.c_str(), message.length(), 0) < 0) {
-        std::cerr << "Error: failed to send message" << std::endl;
-        return 1;
+        int valread = read(sock, buffer, 1024);
+        if (valread < 0)
+        {
+            std::cerr << "Error: failed to receive response" << std::endl;
+            return 1;
+        }
+        std::cout << "Server response: " << buffer << std::endl;
+        sleep(1);
     }
-    std::cout << "Message sent to server" << std::endl;
-
-    int valread = read(sock, buffer, 1024);
-    if (valread < 0) {
-        std::cerr << "Error: failed to receive response" << std::endl;
-        return 1;
-    }
-    std::cout << "Server response: " << buffer << std::endl;
-
     close(sock);
     return 0;
 }
